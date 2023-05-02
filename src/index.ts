548e70486116ace0c11d254a6041280a7689e714
Harsh-Aghera <harsh@isummation.com>
2023-05-02 09:41:24 +0530

    Addedd Controller

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,10 @@
-import express, { Express, Request, Response, NextFunction } from "express";
-import Joi from "joi";
+import express, { Express} from "express";
 import { router } from './routes/routes'
+import {middleware} from './controller/middleware'
 const PORT: number = 5000;
 const app: Express = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/books',router)
-
-const middleware = function (
-  req: Request,
-  res: Response,
-  next: NextFunction
-): void {
-  const Schema = Joi.object({
-    author: Joi.string()
-      .required()
-      .pattern(/^[a-zA-Z0-9, ]/)
-      .max(30),
-    title: Joi.string()
-      .required()
-      .pattern(/^[a-zA-Z0-9, ]/)
-      .max(30),
-    reviews: Joi.string()
-      .pattern(/^[a-zA-Z0-9, ]/)
-      .max(100)
-      .allow(""),
-  });
-  if (req.method === "POST" || req.method === "PUT") {
-    const { error, value } = Schema.validate(req.body, {
-      abortEarly: false,
-    });
-    if (error) {
-      res.send(error.details);
-    }
-  }
-  next();
-};
 app.use(middleware);
+app.use('/books',router)
 app.listen(PORT, () => console.log("Server is running"));
